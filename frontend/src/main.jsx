5681f60f5226e022f01d6e3a0aa1c5ebcae3ff10
sudhirkrsingh0601 <mr.sudhirkumarsingh8@gmail.com>
2025-04-16 10:21:08 +0530

    about and logout page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { UserContextProvider } from "./context/UserContext.jsx";
+import { CourseContextProvider } from "./context/CourseContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export const server = "http://localhost:5000";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <UserContextProvider>
+      <CourseContextProvider>
+        <App />
+      </CourseContextProvider>
+    </UserContextProvider>
+  </React.StrictMode>
+);
